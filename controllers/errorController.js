1ff839823e8b2498bc6bba3d53356474b8382d15
morrisseyjsean <morrisseyjsean@gmail.com>
2020-02-11 18:51:20 +0800

    Split prod/dev envs. Added mongoose error handlers

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,62 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldDB = err => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}, Please use another value.`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data. ${errors.join(' ')}`;
+  return new AppError(message, 400);
+};
+
+const sendDevError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    stack: err.stack,
+    message: err.message
+  });
+};
+
+const sendProdError = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    // Programming or unknown errors. Don't leak to users.
+    // 1) Log error
+    console.error('ERROR 💥', err);
+    // 2) Send message to user
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong! Please contact the webmaster.'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   // Set defaults
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendDevError(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    // Manage Mongoose errors
+    let error = { ...err };
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateFieldDB(err);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
+    sendProdError(error, res);
+  }
 };
