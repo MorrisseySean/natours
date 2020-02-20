74ff005544e44e69ce276265e66bd58b4268b10c
morrisseyjsean <morrisseyjsean@gmail.com>
2020-02-20 16:44:50 +0800

    Implemented rate limits w/ express-rate-limit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 // Router modules
@@ -13,8 +13,15 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); // Only run logging in development
 }
-app.use(express.json());
+// Only allow 100 requests per hour per IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP. Please try again later.'
+});
+app.use('/api', limiter);
 
+app.use(express.json());
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
