d8cfdf2365d7acf88856ce028a8fdfd5f669f69c
morrisseyjsean <morrisseyjsean@gmail.com>
2020-02-24 17:07:03 +0800

    Added route to get distance to all tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const Tour = require('./../models/tourModel');
 const catchASync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
 
 exports.aliasTopTours = (req, res, next) => {
@@ -76,5 +77,67 @@ exports.getMonthlyPlan = catchASync(async (req, res, next) => {
   res.status(200).json({
     status: 'success',
     data: plan
+  });
+});
+
+exports.getToursWithin = catchASync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+  const radius = unit === ',mi' ? distance / 3963.2 : distance / 6378.1;
+
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        400,
+        'Please provide longitute & latitude in the format lat, lng'
+      )
+    );
+  }
+
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+  });
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: tours
+  });
+});
+
+exports.getDistances = catchASync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        400,
+        'Please provide longitute & latitude in the format lat, lng'
+      )
+    );
+  }
+  const div = unit === 'mi' ? 0.000621371 : 0.001;
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1]
+        },
+        distanceField: 'distance',
+        distanceMultiplier: div
+      }
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1
+      }
+    }
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: distances
   });
 });
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -24,7 +24,11 @@ router
     authController.restrictTo('admin', 'lead-guide', 'guide'),
     tourController.getMonthlyPlan
   );
+router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
 
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
 // Tours routes
 router
   .route('/')
