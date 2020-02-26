13d7ea33b013f3235858b8b441903c4e87735749
morrisseyjsean <morrisseyjsean@gmail.com>
2020-02-26 18:56:58 +0800

    Added error pages

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -22,32 +22,55 @@ const handleJWTError = () =>
 const handleJWTExpired = () =>
   new AppError('Your login has timed out. Please log in again.', 401);
 
-const sendDevError = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    stack: err.stack,
-    message: err.message
-  });
-};
-
-const sendProdError = (err, res) => {
-  if (err.isOperational) {
+const sendDevError = (err, req, res) => {
+  if (req.originalUrl.startsWith('/api')) {
     res.status(err.statusCode).json({
       status: err.status,
+      error: err,
+      stack: err.stack,
       message: err.message
     });
   } else {
-    // Programming or unknown errors. Don't leak to users.
+    res.status(err.statusCode).render('error', {
+      title: 'Something went wrong!',
+      msg: err.message
+    });
+  }
+};
+
+const sendProdError = (err, req, res) => {
+  // Operational errors to share to the user
+  if (err.isOperational) {
+    // API
+    if (req.originalUrl.startsWith('/api')) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+      });
+    }
+    // Rendered
+    return res.status(err.statusCode).render('error', {
+      title: 'Something went wrong!',
+      msg: err.message
+    });
+  }
+  // Programming or unknown errors. Don't leak to users.
+  // API
+  if (req.originalUrl.startsWith('/api')) {
     // 1) Log error
     // eslint-disable-next-line no-console
     console.error('ERROR 💥', err);
     // 2) Send message to user
-    res.status(500).json({
+    return res.status(500).json({
       status: 'error',
       message: 'Something went wrong! Please contact the webmaster.'
     });
   }
+  // Rendered
+  res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: 'Something went very wrong! Please contact the administrator'
+  });
 };
 
 module.exports = (err, req, res, next) => {
@@ -56,15 +79,16 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendDevError(err, res);
+    sendDevError(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
     // Manage Mongoose errors
     let error = { ...err };
+    error.message = err.message;
     if (err.name === 'CastError') error = handleCastErrorDB(error);
     if (err.code === 11000) error = handleDuplicateFieldDB(error);
     if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
     if (err.name === 'JsonWebTokenError') error = handleJWTError();
     if (err.name === 'TokenExpiredError') error = handleJWTExpired();
-    sendProdError(error, res);
+    sendProdError(error, req, res);
   }
 };
